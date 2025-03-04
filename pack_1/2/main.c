7d7894cc0a0a5f7308ce8f56c662de1682136ebd
Daniil Makhov <mahovdnl@gmail.com>
2025-03-05 01:25:18 +0300

    some changes

--- pack_1/2/main.c
+++ pack_1/2/main.c
@@ -1,10 +1,34 @@
 #include <stdio.h>
 
+typedef enum errCodes{
+    SUCCESS,
+    TOO_FEW_PARAMS_ERR,
+    NO_FLAG_ERR,
+    FILE_OPEN_ERR,
+    MEM_ALLOC_ERR,
+
+} errCodes;
+
+typedef enum flagOptions{
+    XOR_N, 
+    MASK,
+    COPY_N,
+    FIND,
+}flagOptions;
+
+errCodes parse_argv(int argc, char ** argv, flagOptions * flag){
+    if (argc < 3) return TOO_FEW_PARAMS_ERR;
+    
+    const int valid_n_values_xor = {2, 3, 4, 5, 6};
+
+    if (argv[argc - 1] == "xorN")
+
+    return SUCCESS;
+}
 
 
 int main(int argc, char ** argv){
 
-    printf("Hello, world!\n");
 
     return 0;
 }
